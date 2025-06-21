335a161804ff285dbaf108cfb7b36938b5addaf5
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-21 22:30:15 +0900

    feat: created train.js to do tasks

---
+++ burak/src/train.js
@@ -0,0 +1,16 @@
+/*
+TASK G:
+
+Yagona parametrga ega function tuzing.
+Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1
+
+*/
+
+function getHighestIndex(...number) {
+
+    return Math.max(...number)
+};
+console.log(getHighestIndex(4, 32, 21, 12))
