2a5484e62b13d21a8e8fd1657fa436f3e69c92f4
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-22 16:34:19 +0900

    fix: fixed mistake in g-task

--- burak/src/train.js
+++ burak/src/train.js
@@ -11,6 +11,7 @@ MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1
 
 function getHighestIndex(...number) {
 
-    return Math.max(...number)
+    const maxIndex = Math.max(...number)
+    return number.indexOf(maxIndex)
 };
 console.log(getHighestIndex(4, 32, 21, 12))
