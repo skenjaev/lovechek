3c4b971f19c604ee18c94cece9a34cca944cddfa
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-24 05:05:46 +0900

    feat:added h-task

--- burak/src/train.ts
+++ burak/src/train.ts
@@ -9,9 +9,24 @@ MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1
 
 */
 
-function getHighestIndex(...number) {
+// function getHighestIndex(...number) {
 
-    const maxIndex = Math.max(...number)
-    return number.indexOf(maxIndex)
-};
-console.log(getHighestIndex(4, 32, 21, 12))
+//     const maxIndex = Math.max(...number)
+//     return number.indexOf(maxIndex)
+// };
+// console.log(getHighestIndex(4, 32, 21, 12))
+
+/*
+H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+@MITASK
+*/
+
+function getPositive(numbers: number[]): string {
+    return numbers.filter((num: number) => num > 0).join('');
+}
+
+console.log(getPositive([1, -4, 2]))
