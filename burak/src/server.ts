f80fffe24721c0d1916560cb9ec4a82b066a71ad
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-24 04:13:08 +0900

    feat: connect mongoDB via mongoose

--- burak/src/server.ts
+++ burak/src/server.ts
@@ -1,4 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
-console.log('PORT', process.env.PORT);
-console.log('URL:', process.env.MONGO_URL)
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log('MONGODB connected successfully');
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => {
+        console.log("Error on connection MongoDB ", err)
+    })
