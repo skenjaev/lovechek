5fae25df13b53995e8aacdc12450b6701ba744e4
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-24 04:43:53 +0900

    feat: express integration

--- burak/src/server.ts
+++ burak/src/server.ts
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import mongoose from 'mongoose';
+import app from "./app"
+
 
 mongoose.connect(process.env.MONGO_URL as string, {})
     .then((data) => {
         console.log('MONGODB connected successfully');
-        const PORT = process.env.PORT ?? 3003;
+        const PORT = process.env.PORT ?? 3004;
+        app.listen(PORT, function () {
+            console.log(`The server is running on port: ${PORT}`)
+        })
     })
     .catch((err) => {
         console.log("Error on connection MongoDB ", err)
