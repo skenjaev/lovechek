afbf5b34978f91d4d8f940b780645837b21da503
skenjaev <sunnatullokenjaev8@gmail.com>
2025-06-26 18:36:19 +0900

    feat:created routes and member controllers

--- burak/src/app.ts
+++ burak/src/app.ts
@@ -1,13 +1,22 @@
 import express from 'express'
 import path from 'path'
-
+import router from './router';
 
 //1 -- ENTERANCE --;
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
+// 2- sessions;
+
+
+// 3- views
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
+
+
+//4- routers
+app.use('/', router)
 
 export default app;
