74bfe97cb827afa2d32fb8c2d369c4cb1dac334d
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-06 18:42:05 +0900

    feat: fixed routing err

--- burak/src/controllers/member.controller.ts
+++ burak/src/controllers/member.controller.ts
@@ -15,6 +15,7 @@ memberController.signup = async (req: Request, res: Response) => {
 
         const input: MemberInput = req.body,
         result: Member = await memberService.signup(input);
+        
         res.json({member: result});
     }
     catch (err) {
--- burak/src/router.ts
+++ burak/src/router.ts
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 import memberController from './controllers/member.controller';
 
-router.get('/login', memberController.login)
-router.get('/signup', memberController.signup)
+router.post('/login', memberController.login)
+router.post('/signup', memberController.signup)
 
 
 
