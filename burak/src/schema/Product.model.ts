1d98ea5319958eabc652f7ba9f29bfde39a7003d
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-10 22:10:40 +0900

    fix: saved schema

---
+++ burak/src/schema/Product.model.ts
@@ -0,0 +1,74 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductCollection,
+  ProductSize,
+  ProductStatus,
+  ProductVolume,
+} from "../libs/enums/product.enum";
+
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: Object.values(ProductStatus),
+      default: ProductStatus.PAUSE,
+    },
+
+    productCollection: {
+      type: String,
+      enum: Object.values(ProductCollection),
+      required: true,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+
+    productSize: {
+      type: String,
+      enum: Object.values(ProductSize),
+      default: ProductSize.NORMAL,
+    },
+
+    productVolume: {
+      type: String,
+      enum: Object.values(ProductVolume),
+      default: ProductVolume.ONE,
+    },
+
+    productDesc: {
+      type: String,
+      required: true,
+    },
+
+    productImages: {
+      type: [String],
+      default: [],
+    },
+
+    productViews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } // createdAt va updatedAt qo‘shiladi
+);
+
+// Takrorlanmas kombinatsiya
+productSchema.index(
+  { productName: 1, productSize: 1, productVolume: 1 },
+  { unique: true }
+);
+
+export default mongoose.model("Product", productSchema);
