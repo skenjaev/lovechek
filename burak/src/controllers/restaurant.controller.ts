91511596476be9342a4afe8bcb76eb398907e5a7
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-16 19:35:07 +0900

    fix: correction of processLogin logic

--- burak/src/controllers/restaurant.controller.ts
+++ burak/src/controllers/restaurant.controller.ts
@@ -64,7 +64,7 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
     catch (err) {
         console.log('Error, processSignup', err)
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-        res.send(`<script>alert("${message}");window.location.replace("admin/signup")</script>`)
+        res.send(`<script>alert("${message}");window.location.replace("/admin/signup")</script>`)
     }
 }
 
@@ -88,7 +88,7 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
     catch (err) {
         console.log('Error, processLogin', err)
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-        res.send(`<script>alert("${message}");window.location.replace("admin/login")</script>`)
+        res.send(`<script>alert("${message}");window.location.replace("/admin/login")</script>`)
     }
 };
 
