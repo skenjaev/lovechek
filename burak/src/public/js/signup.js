bfbeb6c77aeaef234987e178d1382683695bf292
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-17 15:58:55 +0900

    feat:signup process frontend validation

--- burak/src/public/js/signup.js
+++ burak/src/public/js/signup.js
@@ -1,1 +1,26 @@
 console.log("Signup frontend javascript file");
+
+
+$(function () { });
+
+function validateSignupForm() {
+    const memberNick = $('.member-nick').val();
+    const memberPhone = $('.member-phone').val();
+    const memberPassword = $('.member-password').val();
+    const confirmPassword = $('.member-password').val();
+
+    if (
+        memberNick === '' ||
+        memberPhone === '' ||
+        memberPassword === '' ||
+        confirmPassword === ''
+    ) {
+        alert('Plese insert all required fields');
+        return false;
+    }
+
+    if (confirmPassword !== memberPassword) {
+        alert('Passwords do not match. Please try again.')
+        return false;
+    }
+}
