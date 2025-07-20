7336007178a6b51ca413dd2541b350c8bd59dd96
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-20 18:06:39 +0900

    fix: modify spa login api process

--- burak/src/libs/Errors.ts
+++ burak/src/libs/Errors.ts
@@ -16,6 +16,7 @@ export enum Message {
     NO_DATA_FOUND = "No data is found!",
     CREATE_FAILED = "Create is failed!",
     UPDATE_FAILED = "Update is failed!",
+    BLOCKED_USER = "You have been blocked, please contact restaurant!",
     USED_NICK_PHONE= " you are inserting already used nick or phone!",
     NO_MEMBER_NICK = "no member with this member nick!",
     WRONG_PASSWORD = "wrong password,please try again!",
