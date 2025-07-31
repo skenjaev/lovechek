7920a5632999df3259caa3f56ec7137fccd59779
skenjaev <sunnatullokenjaev8@gmail.com>
2025-07-31 20:51:36 +0900

    fix: added task T

--- burak/src/train.ts
+++ burak/src/train.ts
@@ -1,3 +1,21 @@
+// T-TASK:
+
+// Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+
+// @MITASK
+
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+
+
+
+
+
 
 // S-TASK:
 
@@ -6,13 +24,13 @@
 
 // @MITASK
 
-function missingNumber(numbers: number[]): number {
-  for (let i = 0; i <= numbers.length; i++) {
-    if (!numbers.includes(i)) return i;
-  }
-  return 1
-}
-console.log(missingNumber([3, 0, 1, ]))
+// function missingNumber(numbers: number[]): number {
+//   for (let i = 0; i <= numbers.length; i++) {
+//     if (!numbers.includes(i)) return i;
+//   }
+//   return 1
+// }
+// console.log(missingNumber([3, 0, 1, ]))
 
 
 // R-TASK:
