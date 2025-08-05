0c4dca20805491e1e827223802d327d9ce9a5f04
skenjaev <sunnatullokenjaev8@gmail.com>
2025-08-05 18:31:33 +0900

    fix: modify getMemberDetail logic

--- burak/src/controllers/member.controller.ts
+++ burak/src/controllers/member.controller.ts
@@ -72,6 +72,20 @@ memberController.logout = async (req: ExtentedRequest, res: Response, next: Next
 
 }
 
+memberController.getMemberDetail = async (req: ExtentedRequest, res: Response, next: NextFunction) => {
+    try {
+        console.log("getMemberDetail")
+        const result = await memberService.getMemberDetail(req.member);
+        res.status(HttpCode.OK).json(result)
+    }
+    catch (err) {
+        console.log('Error, getMemberDetail', err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard)
+    }
+
+}
+
     memberController.verifyAuth = async (req: ExtentedRequest, res: Response, next: NextFunction) => {
         try {
             const token = req.cookies["accessToken"];
