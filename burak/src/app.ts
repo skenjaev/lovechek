d2b1c8c358629e22d991ed6b3efd1e694a220e96
skenjaev <sunnatullokenjaev8@gmail.com>
2025-08-05 16:58:03 +0900

    feat: develop authentication via tokens with cookies

--- burak/src/app.ts
+++ burak/src/app.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import router from './router';
 import routerAdmin from './router-Admin'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser';
 import { MORGAN_FORMAT } from './libs/config';
 
 import session from 'express-session';
@@ -23,6 +24,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser())
 app.use(morgan(MORGAN_FORMAT))
 
 // 2- sessions;
--- burak/src/controllers/member.controller.ts
+++ burak/src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import { T } from "../libs/types/common";
 import MemberService from '../models/Member.service';
 import {LoginInput, Member, MemberInput } from '../libs/types/member';
-import Errors from '../libs/Errors';
+import Errors, { HttpCode } from '../libs/Errors';
 import AuthService from '../models/Auth.service';
+import { AUTH_TIMER } from '../libs/config';
 
 const memberController: T = {};
 
@@ -18,8 +19,13 @@ memberController.signup = async (req: Request, res: Response) => {
         const input: MemberInput = req.body,
         result: Member = await memberService.signup(input),
         token =await authService.createToken(result);
-        console.log("token:", token)
-        res.json({member: result});
+        res.cookie("accessToken", token,{
+            maxAge: AUTH_TIMER*3600*1000,
+            httpOnly:false
+        })
+
+
+        res.status(HttpCode.CREATED).json({member: result, accessToken: token});
     }
     catch (err) {
         console.log('Error, signup', err);
@@ -36,8 +42,14 @@ memberController.login = async (req: Request, res: Response) => {
         const input: LoginInput = req.body,
         result = await memberService.login(input),
         token =await authService.createToken(result);
-        console.log("token:", token)
-        res.json({member: result});
+
+        res.cookie("accessToken", token,{
+            maxAge: AUTH_TIMER*3600*1000,
+            httpOnly:false
+        })
+
+
+        res.status(HttpCode.OK).json({member: result, accessToken: token});
     }
     catch (err) {
         console.log('Error, login', err);
