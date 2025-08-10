c42df89baa0f36211e6169dbe2599f1be82da87a
skenjaev <sunnatullokenjaev8@gmail.com>
2025-08-10 18:22:39 +0900

    fix: modify getProduct logic

--- burak/src/models/Product.service.ts
+++ burak/src/models/Product.service.ts
@@ -5,11 +5,17 @@ import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enum";
 import { T } from "../libs/types/common";
 import {ObjectId} from "mongoose";
+import ViewService from "./Views.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view..enum";
 
 class ProductService {
     private readonly productModel;
+    public viewService;
+
     constructor(){
         this.productModel = ProductModel
+        this.viewService = new ViewService();
     }
 
     /* SPA */
@@ -50,6 +56,32 @@ class ProductService {
         .exec()
         if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
         
+        if(memberId){
+            //check exictance
+            const input: ViewInput = {
+                memberId: memberId,
+                viewRefId: productId,
+                viewGroup: ViewGroup.PRODUCT
+            }
+
+            const existView = await this.viewService.checkViewExictance(input);
+            console.log("exist:", !!existView);
+            if(!existView) {
+                //insert view
+                console.log("New views inserting");
+                await this.viewService.insertMemberView(input);
+
+                //increase counts logic
+                result = await this.productModel
+                .findByIdAndUpdate(
+                    productId,
+                    {$inc: {productViews: +1}},
+                    {new: true}
+                )
+                .exec()
+            }
+        }
+
         return result
     }
 
