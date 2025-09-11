f280133a2ec638e3c8900839a0143fd29711d025
skenjaev <sunnatullokenjaev8@gmail.com>
2025-09-11 20:24:04 +0900

    fix: added MTask

--- burak/src/train.ts
+++ burak/src/train.ts
@@ -1,10 +1,25 @@
-function delayHelloWorld(a: string): Promise<string> {
-  return new Promise((resolve) => {
-      setTimeout(() => {
-          resolve(a);
-      }, 3000)
-  });
+function reduceNestedArray(myArr: any[]): number {
+  let sum = 0;
+  for (const val of myArr) {
+    if (Array.isArray(val)) {
+      sum += reduceNestedArray(val); 
+    } else {
+      sum += val; 
+    }
+  }
+  return sum;
 }
 
-delayHelloWorld("Hello World").then((result) => {
-  console.log(result)});
+console.log(reduceNestedArray([1, [1, 2, [4]]])); 
+
+
+// function delayHelloWorld(a: string): Promise<string> {
+//   return new Promise((resolve) => {
+//       setTimeout(() => {
+//           resolve(a);
+//       }, 3000)
+//   });
+// }
+
+// delayHelloWorld("Hello World").then((result) => {
+//   console.log(result)});
