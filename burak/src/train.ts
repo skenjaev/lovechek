2e38a26e073b068141d2c6d0a993500c1c69efe9
skenjaev <sunnatullokenjaev8@gmail.com>
2025-10-02 21:01:34 +0900

    fix: MTask

--- burak/src/train.ts
+++ burak/src/train.ts
@@ -1,14 +1,8 @@
-function findDuplicates(arr: number[]): number[] {
-  const getDouble: number[] = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    const count = arr.filter(x => x === arr[i]).length;
-
-    if (count >= 2 && !getDouble.includes(arr[i])) {
-      getDouble.push(arr[i]);
+function singleNumber(nums: number[]): number | undefined {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) {
+      return nums[i];
     }
   }
-
-  return getDouble;
 }
-console.log(findDuplicates([1, 2, 3, 4, 5, 4, 3, 4])); 
+console.log(singleNumber([4, 2, 1, 2, 1]))
