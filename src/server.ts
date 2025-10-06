fea2961ac9ff9adcb68778c863bcbf8cbb887ab9
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-10-06 13:07:10 +0900

    feat: integration of environmential variables

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ mongoose
 .connect(process.env.MONGO_URL as string, {})
 .then((data) => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 3028;
     app.listen(PORT, function() {
         console.info(`The server is running successfully on port: ${PORT}`);
         console.info(`Admin project on http://localhost:${PORT}/admin \n`);
